2e207470cb71aab1187f4e207a0dcaa86d28a606
SmartPolarBear <clevercoolbear@outlook.com>
2022-07-15 16:48:15 +0800

    feat(converter): add laz converter

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from aligntree.registration import registrate
+from converter import read_laz_point_cloud
 
+import open3d as o3d
 
+pcd = read_laz_point_cloud('data/uls.laz')
+o3d.visualization.draw_geometries([pcd])
